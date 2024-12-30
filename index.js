0fb43a52b4ee8be9a6611ce562c610f9b12b9f00
ujaann <ujanst@gmail.com>
2024-12-30 21:41:16 +0545

    Added mongoose package

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express=require('express');
 const userRoutes=require('./routes/user_route');
+const connectDB =require('./config/db');
+
 
 const app=express();
 
 const PORT=5000;
-app.listen(PORT,()=>{   
-    console.log(`App running in port: ${PORT}`);
-    
-})
 
 app.use(express.json());
 
-app.get('/',(req,res)=>res.json({"message":"Hello World"}));
+app.use((req,res,next)=>{
+    console.log(req.path, req.method);
+    next();
+})
 
-app.use('/user',userRoutes);
+connectDB().then(()=>{
+    app.listen(PORT,()=>{   
+        console.log(`App running in port: ${PORT}`); 
+    });
+});
+
+
+app.use('/api/user',userRoutes);
