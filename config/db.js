ca3b340cde402291646edb331de9e404796a6720
ujaann <ujanst@gmail.com>
2025-01-29 18:36:55 +0545

    added .env to hide databse

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose=require("mongoose");
+require('dotenv').config()
+
 const connectDB=async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/db_recipe_application");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected :)");
     } catch (error) {
         console.log("MongoDB not connected :(");
--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const express=require('express');
-const userRoutes=require('./routes/UserRoutes');
-const recipeRoutes=require('./routes/RecipeRoutes');
-const connectDB =require('./config/db');
+const express = require("express");
+const userRoutes = require("./routes/UserRoutes");
+const recipeRoutes = require("./routes/RecipeRoutes");
+const connectDB = require("./config/db");
 
+const app = express();
 
-const app=express();
+const PORT = 5000;
 
-const PORT=5000;
-
-connectDB().then(()=>{
-    app.listen(PORT,()=>{   
-        console.log(`App running in port: ${PORT}`); 
-    });
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`App running in port: ${PORT}`);
+  });
 });
 
 app.use(express.json());
 
-app.use((req,res,next)=>{
-    console.log(req.path, req.method);
-    next();
-})
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-
-app.use('/api/user',userRoutes);
-app.use('/api/recipe',recipeRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/recipe", recipeRoutes);
